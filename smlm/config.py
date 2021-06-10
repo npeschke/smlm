05ff4c2cb4c65a8284c1954cf1d6f766d2b940f7
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2021-06-10 14:04:04 +0200

    added stage separated r/density plot

--- smlm/config.py
+++ smlm/config.py
@@ -1,6 +1,5 @@
-# import matplotlib.colors as mpl_col
 import seaborn as sns
-# import cmasher
+import cmasher
 
 # COLORS
 # SNS_BLUE = (0.12156862745098039, 0.4666666666666667, 0.7058823529411765)
@@ -9,8 +8,17 @@ SNS_BLUE = "#4c72b0"
 # APOPTOSIS_RED = (1.0, 0.0, 0.0)
 APOPTOSIS_RED = "#ff0000"
 
-# cmap = cmasher.get_sub_cmap(sns.color_palette(as_cmap=True), 0, 1,)
-
 # CMAPS
-# BIN_CMAP = mpl_col.LinearSegmentedColormap.from_list(name="apoptosis", colors=[SNS_BLUE, APOPTOSIS_RED], N=2)
 BIN_CMAP = sns.color_palette([SNS_BLUE, APOPTOSIS_RED])
+
+SEQ_CMAP = cmasher.chroma
+
+# Figure Parameters
+FIG_BASE_SIZE = 6.5
+
+# Limits
+DENSITY_LIM = (1e-4, 1e0)
+RADIUS_LIM = (0, 8000)
+
+CBAR_MIN = 1e-5
+CBAR_MAX = 3e-4
--- smlm/plot.py
+++ smlm/plot.py
@@ -1,12 +1,13 @@
+import cmasher
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-import cmasher
+from ..smlm import config as smlm_config
 
 
 def get_n_stages(method_name: str, stage_df: pd.DataFrame):
-    return len(stage_df[method_name].unique())
+    return len(stage_df[method_name].dropna().unique())
 
 
 # def get_n_stages(method: str, data: pd.DataFrame):
@@ -57,7 +58,7 @@ def plot_joint_fig(method: str, data: pd.DataFrame, stage_df: pd.DataFrame, cell
                                        figsize=(joint_columns * figure_base_size * 1.2, joint_rows * figure_base_size),
                                        sharex="col", sharey="col", dpi=dpi)
     for stage_idx in range(joint_rows):
-        stage_cells = data.loc[data[method] == stage_idx + 1]
+        stage_cells = get_stage_data(data, method, stage_idx + 1)
 
         with sns.axes_style("white"):
             joint_ax[stage_idx][0] = sns.histplot(x="x", y="y", data=data.loc[data.file == cells_to_plot[stage_idx]],
@@ -75,16 +76,22 @@ def plot_joint_fig(method: str, data: pd.DataFrame, stage_df: pd.DataFrame, cell
         joint_ax[stage_idx][1].set_xlim(*voronoi_density_lims)
         joint_ax[stage_idx][1].set_xlabel(r"Voronoi Density $\left[\frac{1}{nm^2}\right]$")
 
-        joint_ax[stage_idx][2] = sns.histplot(x="density", y="r", data=stage_cells, ax=joint_ax[stage_idx][2],
-                                              log_scale=(True, False),
-                                              cbar=True,
-                                              stat="probability",
-                                              cmap=cmap,
-                                              vmin=color_vmin, vmax=color_vmax, thresh=color_vmin)
-        joint_ax[stage_idx][2].set_xlabel(r"Voronoi Density $\left[\frac{1}{nm^2}\right]$")
-        joint_ax[stage_idx][2].set_ylabel("Radius [nm]")
-        joint_ax[stage_idx][2].set_xlim(*voronoi_density_lims)
-        joint_ax[stage_idx][2].set_ylim(*radius_lims)
+        joint_ax[stage_idx][2] = plot_r_density_dist(data=stage_cells,
+                                                     density_lim=voronoi_density_lims,
+                                                     radius_lim=radius_lims,
+                                                     cbar_min=color_vmin, cbar_max=color_vmax,
+                                                     ax=joint_ax[stage_idx][2])
+
+        # joint_ax[stage_idx][2] = sns.histplot(x="density", y="r", data=stage_cells, ax=joint_ax[stage_idx][2],
+        #                                       log_scale=(True, False),
+        #                                       cbar=True,
+        #                                       stat="probability",
+        #                                       cmap=cmap,
+        #                                       vmin=color_vmin, vmax=color_vmax, thresh=color_vmin)
+        # joint_ax[stage_idx][2].set_xlabel(r"Voronoi Density $\left[\frac{1}{nm^2}\right]$")
+        # joint_ax[stage_idx][2].set_ylabel("Radius [nm]")
+        # joint_ax[stage_idx][2].set_xlim(*voronoi_density_lims)
+        # joint_ax[stage_idx][2].set_ylim(*radius_lims)
     joint_ax[0][0].set_title("Example Nucleus")
     joint_ax[0][1].set_title("Density Distribution")
     joint_ax[0][2].set_title("Radius/Density\nDistribution")
@@ -92,6 +99,10 @@ def plot_joint_fig(method: str, data: pd.DataFrame, stage_df: pd.DataFrame, cell
     joint_fig.suptitle(method_display)
     joint_fig.tight_layout()
     return joint_fig
+
+
+def get_stage_data(data, method, stage) -> pd.DataFrame:
+    return data.loc[data[method] == stage]
 
 
 def plot_binary_joint_fig(method: str, data: pd.DataFrame, stage_df: pd.DataFrame, cells_to_plot: list, labelling: str, method_display: str, dpi: int = 300):
@@ -132,16 +143,22 @@ def plot_binary_joint_fig(method: str, data: pd.DataFrame, stage_df: pd.DataFram
         joint_ax[stage_idx][1].set_xlim(*voronoi_density_lims)
         joint_ax[stage_idx][1].set_xlabel(r"Voronoi Density $\left[\frac{1}{nm^2}\right]$")
 
-        joint_ax[stage_idx][2] = sns.histplot(x="density", y="r", data=stage_cells, ax=joint_ax[stage_idx][2],
-                                              log_scale=(True, False),
-                                              cbar=True,
-                                              stat="probability",
-                                              cmap=cmap,
-                                              vmin=color_vmin, vmax=color_vmax, thresh=color_vmin)
-        joint_ax[stage_idx][2].set_xlabel(r"Voronoi Density $\left[\frac{1}{nm^2}\right]$")
-        joint_ax[stage_idx][2].set_ylabel("Radius [nm]")
-        joint_ax[stage_idx][2].set_xlim(*voronoi_density_lims)
-        joint_ax[stage_idx][2].set_ylim(*radius_lims)
+        joint_ax[stage_idx][2] = plot_r_density_dist(data=stage_cells,
+                                                     density_lim=voronoi_density_lims,
+                                                     radius_lim=radius_lims,
+                                                     cbar_min=color_vmin, cbar_max=color_vmax,
+                                                     ax=joint_ax[stage_idx][2])
+
+        # joint_ax[stage_idx][2] = sns.histplot(x="density", y="r", data=stage_cells, ax=joint_ax[stage_idx][2],
+        #                                       log_scale=(True, False),
+        #                                       cbar=True,
+        #                                       stat="probability",
+        #                                       cmap=cmap,
+        #                                       vmin=color_vmin, vmax=color_vmax, thresh=color_vmin)
+        # joint_ax[stage_idx][2].set_xlabel(r"Voronoi Density $\left[\frac{1}{nm^2}\right]$")
+        # joint_ax[stage_idx][2].set_ylabel("Radius [nm]")
+        # joint_ax[stage_idx][2].set_xlim(*voronoi_density_lims)
+        # joint_ax[stage_idx][2].set_ylim(*radius_lims)
     joint_ax[0][0].set_title("Example Nucleus")
     joint_ax[0][1].set_title("Density Distribution")
     joint_ax[0][2].set_title("Radius/Density\nDistribution")
@@ -149,6 +166,48 @@ def plot_binary_joint_fig(method: str, data: pd.DataFrame, stage_df: pd.DataFram
     joint_fig.suptitle(method_display)
     joint_fig.tight_layout()
     return joint_fig
+
+
+def plot_r_density_dist_stages(data: pd.DataFrame,
+                               method: str,
+                               dpi: int = 300):
+    cols = get_n_stages(method, data)
+    fig, ax = plt.subplots(ncols=cols,
+                           figsize=(smlm_config.FIG_BASE_SIZE * cols, smlm_config.FIG_BASE_SIZE),
+                           sharex="col", sharey="row",
+                           dpi=dpi)
+
+    for col_idx in range(cols):
+        stage = col_idx + 1
+        ax[col_idx] = plot_r_density_dist(data=get_stage_data(data, method, stage),
+                                          density_lim=smlm_config.DENSITY_LIM,
+                                          radius_lim=smlm_config.RADIUS_LIM,
+                                          cbar_min=smlm_config.CBAR_MIN,
+                                          cbar_max=smlm_config.CBAR_MAX,
+                                          ax=ax[col_idx])
+
+    # for ax in ax[:-1]:
+    #     ax.
+
+    fig.suptitle("Radius/Density Distribution")
+    fig.tight_layout()
+
+    return fig, ax
+
+
+def plot_r_density_dist(data: pd.DataFrame, density_lim: tuple, radius_lim: tuple, cbar_min: float, cbar_max: float, ax: plt.Axes):
+    ax = sns.histplot(x="density", y="r", data=data, ax=ax,
+                      log_scale=(True, False),
+                      cbar=True,
+                      stat="probability",
+                      cmap=smlm_config.SEQ_CMAP,
+                      vmin=cbar_min, vmax=cbar_max, thresh=cbar_min)
+    ax.set_xlabel(r"Voronoi Density $\left[\frac{1}{nm^2}\right]$")
+    ax.set_ylabel("Radius [nm]")
+    ax.set_xlim(*density_lim)
+    ax.set_ylim(*radius_lim)
+
+    return ax
 
 
 def plot_localization_counts(loc_label, method, method_label, data, size, dpi):
