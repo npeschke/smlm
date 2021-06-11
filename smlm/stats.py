2f002207090412e072c401289ceace71a9b36272
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2021-06-11 17:02:07 +0200

    added added counts with sem for aCasp3 signal

--- smlm/stats.py
+++ smlm/stats.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import scipy.stats as scs
 
@@ -36,14 +37,49 @@ def wilcoxon_rank_sums(comparison_column: str, n_stages: int, stage_method: str,
 def localization_counts(localizations: pd.DataFrame, stages: pd.DataFrame,
                         file_label: str, n_loc_label: str,
                         method: str) -> (pd.DataFrame, pd.DataFrame):
-    counts = localizations.value_counts(subset=[file_label], sort=False)
-    counts.name = n_loc_label
 
-    counts = counts.reset_index().set_index(file_label)
+    counts = get_loc_counts_by_file(localizations, file_label, n_loc_label)
 
     count_stages = stages.set_index(file_label).join(counts, on=file_label).reset_index()
 
-    return count_stages.groupby(method)[n_loc_label].describe(), count_stages
+    description = count_stages.groupby(method)[n_loc_label].describe()
+    description = assign_sem(description)
+
+    return description, count_stages
+
+
+def assign_sem(data: pd.DataFrame, sd_col: str = "std", n_col: str = "count") -> pd.DataFrame:
+    """
+    Calculates SEM and appends it as a new column.
+
+    :param data: Dataframe, ideally created by using the pd.DataFrame.describe() method.
+    :param sd_col: Name of the column in data containing the standard deviation data
+    :param n_col: Name of the column in data containing the number of observations
+    :return: data with an additional sem column
+    """
+    return data.assign(sem=data[sd_col] / np.sqrt(data[n_col]))
+
+
+def loc_counts_by_column(localizations: pd.DataFrame, column: str,
+                         file_label: str = "file", n_loc_label: str = "n_localizations"):
+    counts = get_loc_counts_by_file(localizations, file_label, n_loc_label)
+
+    joined_counts = localizations.set_index(file_label).join(counts, on=file_label).reset_index()
+
+    description = joined_counts.groupby(column)[n_loc_label].describe()
+
+    description = assign_sem(description)
+
+    return description
+
+
+def get_loc_counts_by_file(localizations: pd.DataFrame,
+                           file_label: str = "file",
+                           n_loc_label: str = "n_localizations"):
+    counts = localizations.value_counts(subset=[file_label], sort=False)
+    counts.name = n_loc_label
+    counts = counts.reset_index().set_index(file_label)
+    return counts
 
 
 def describe_hist_distribution(data: pd.DataFrame, desc_col: str, group: str = None) -> pd.DataFrame:
